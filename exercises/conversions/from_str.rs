dfed4650ac8cf454b4844ee7f1bf2fd248e98014
Lilith Screws <lilith@queercoded.io>
2022-09-22 23:30:09 -0400

    Made it to the finish line

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let (name, age) = match s.split(',').collect::<Vec<&str>>()[..] {
+            [name, age] => (name.to_string(), age.parse().map_err(ParsePersonError::ParseInt)?),
+            _ => return Err(ParsePersonError::BadLen),
+        };
+
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        Ok(Person {
+            name: name.into(),
+            age,
+        })
     }
 }
 
